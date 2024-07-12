88f9c02a97c923983ff8a148547675fb1d76e807
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-11 21:09:38 -0400

    Time: 184 ms (23.54%) | Memory: 12.7 MB (77.92%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return -1
+        elif len(nums) == 1:
+            return 0 if nums[0] == target else -1
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1      
+        return -1  
