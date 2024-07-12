b66e6ee4de85152e604f17004c81b8853eff4ace
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-11 21:09:58 -0400

    Time: 185 ms (19.09%) | Memory: 13 MB (15.03%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -5,10 +5,6 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        if len(nums) == 0:
-            return -1
-        elif len(nums) == 1:
-            return 0 if nums[0] == target else -1
         low = 0
         high = len(nums) - 1
         while low <= high:
