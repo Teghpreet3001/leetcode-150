4ab9f7603782f24e04c2fc83d6b0d173afc17ed7
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-11 21:36:05 -0400

    Time: 538 ms (27.40%) | Memory: 22.1 MB (43.88%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxArea(self, heights):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        '''
+            whatever value is smaller, we increment that since we want 
+            to find the maximum area from both the ends, by not moving
+            the larger value, we are preserving it by taking the maximum 
+        '''
+        left = 0 
+        right = len(heights) - 1
+        result = - float('inf')
+        while left < right:
+            area = (right - left)*min(heights[left], heights[right])
+            result = max(result, area)
+            if heights[left] < heights[right]:
+                left += 1
+            elif heights[left] > heights[right]:
+                right -= 1
+            else: 
+                right -= 1
+        return result
