a5318dab3975270d412fe79a790d5bd909be49a1
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-13 20:28:59 -0400

    Time: 23 ms (49.95%) | Memory: 12.1 MB (13.69%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -4,6 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums.sort()
-        return nums[0]
+        result = nums[0]
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+
+            #check if the array is already sorted and return the left
+            if nums[low] < nums[high]:
+                result = min(result, nums[low])
+                break
+
+            mid = (low + high)//2
+            result = min(result, nums[mid])
+            if nums[low] <= nums[mid]: #left portion is sorted so we look at right sorted portion
+                low = mid + 1
+            else: #right portion is sorted so we look at left sorted portion
+                high = mid - 1 
+        return result 
+                
         
