7451a95b18357dfa45ff4c6b1e53345269ef64aa
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-15 22:24:37 -0400

    Time: 770 ms (43.41%) | Memory: 20.9 MB (54.19%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,12 +4,18 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        min_price = prices[0]
-        max_profit = 0
-        
-        for i in range(1, len(prices)):
-            max_profit = max(max_profit, prices[i] - min_price)
-            min_price = min(min_price, prices[i])
-            
-        return max_profit
-        
+        #idea: buy at lowest price, sell at highest
+        left = 0 #buying 
+        right = 1 #selling pointer
+        maxprofit = 0
+        while right < len(prices):
+            #is it profitable
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxprofit = max(maxprofit, profit)
+            else:
+                #set left to the extreme end as we found the lowest value for left and highest value for right
+                left = right
+            #keep moving the right pointer 
+            right += 1
+        return maxprofit
