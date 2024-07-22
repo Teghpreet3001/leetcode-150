10bead27eb2ebd78e3f48d477a1fd80279d6b711
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-21 21:39:04 -0400

    Time: 123 ms (19.06%) | Memory: 13.9 MB (57.56%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -7,27 +7,24 @@ class Solution(object):
         area = 0
         m = len(grid)
         n = len(grid[0])
-        visited = set()
-
-        def calculateArea(grid, i, j):
-            if i < 0 or j < 0 or i >= m or j >= n:
-                return 0
-
-            #check if not an island and check if visited before
-            if grid[i][j] != 1 or (i,j) in visited:
-                return 0
-            visited.add((i,j))
-            directions = [(1,0), (0,1), (-1,0), (0,-1)]
-            localArea = 0
-            for direction in directions:
-                new_i, new_j = i + direction[0], j + direction[1]
-                localArea += calculateArea(grid, new_i, new_j)
-            print(1+localArea)
-            return 1 + localArea
-
         for i in range(m):
             for j in range(n):
-                area = max(area, calculateArea(grid, i, j))
+                area = max(area, self.calculateArea(grid, i, j))
         return area
         
-    
+    def calculateArea(self, grid, i, j):
+        m = len(grid)
+        n = len(grid[0])
+        if i < 0 or j < 0 or i >= m or j >= n:
+            return 0
+        #check if not an island and check if visited before
+        if grid[i][j] != 1 or grid[i][j] == "#":
+            return 0
+        grid[i][j] = "#"
+        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        localArea = 0
+        for direction in directions:
+            new_i, new_j = i + direction[0], j + direction[1]
+            localArea += self.calculateArea(grid, new_i, new_j)
+        print(1+localArea)
+        return 1 + localArea
