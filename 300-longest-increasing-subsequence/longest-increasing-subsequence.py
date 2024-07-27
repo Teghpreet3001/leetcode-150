7282508a20eec74e67c93da2929483185ed5373b
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-27 19:02:28 -0400

    Time: 1748 ms (60.32%) | Memory: 16.8 MB (90.64%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -8,10 +8,8 @@ class Solution:
         n = len(nums)
         #length of LID starting at any index will be 1
         dp =[1]*(n)
-        dp[0] = 1
         for i in range(1, len(nums)):
             for j in range(0,i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
-        print(dp)
         return max(dp) #and not dp[n-1] resent anywsince the longest LIS can end anywhere in the list and beyond that we have smaller numbers
