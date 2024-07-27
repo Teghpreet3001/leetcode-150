7fa0c3e0fdb7cb26db8fb1745415c9e7361dc832
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-27 17:10:01 -0400

    Time: 41 ms (23.54%) | Memory: 16.4 MB (75.23%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        #bottom up dp
+        n = len(s)
+        #dp is a 1D array that stores num ways for a string of dp[i:]
+        #since it is bottom up, we start loop backwards and return dp[0]
+        dp = [1]*(n + 1)
+
+        for i in range(len(s)-1, -1, -1):
+            #base case that the string starts with 0
+            if s[i] == "0":
+                dp[i] = 0
+            else: #string is btw 0 to 9 so we add the next one digit 
+                dp[i] = dp[i + 1]
+            
+            #check if the next two digits are valid and add them
+            #only going to be valid  if starts with 1 or 2 and  second digot is 0,1,2,3,4,5,6 (only for 2) as 26 is the  max value 
+            #for example, 31 is not valid so we do not add that 
+            # To make sure taht we do not run into index out of range, we chcek if i+1 < len(s)
+
+            if (i + 1 < len(s)) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+                dp[i] += dp[i + 2]
+                
+        print(dp)
+        return dp[0]
