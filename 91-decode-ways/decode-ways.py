3c105453f927a0e7dfa1e1a9b6715f18bcd38c11
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-27 17:14:23 -0400

    Time: 33 ms (76.40%) | Memory: 16.6 MB (46.90%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -5,14 +5,15 @@ class Solution:
         n = len(s)
         #dp is a 1D array that stores num ways for a string of dp[i:]
         #since it is bottom up, we start loop backwards and return dp[0]
-        dp = [1]*(n + 1)
+        dp1 = 1
+        dp2 = 0
 
         for i in range(len(s)-1, -1, -1):
             #base case that the string starts with 0
             if s[i] == "0":
-                dp[i] = 0
+                dp = 0
             else: #string is btw 0 to 9 so we add the next one digit 
-                dp[i] = dp[i + 1]
+                dp = dp1
             
             #check if the next two digits are valid and add them
             #only going to be valid  if starts with 1 or 2 and  second digot is 0,1,2,3,4,5,6 (only for 2) as 26 is the  max value 
@@ -20,7 +21,9 @@ class Solution:
             # To make sure taht we do not run into index out of range, we chcek if i+1 < len(s)
 
             if (i + 1 < len(s)) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
-                dp[i] += dp[i + 2]
+                dp += dp2
+            dp2 = dp1
+            dp1 = dp
 
         print(dp)
-        return dp[0]
+        return dp
