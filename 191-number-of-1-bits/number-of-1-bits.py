36aea1e4582ee030442f3af90e3be98156cf03e8
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-07-28 02:41:25 -0400

    Time: 34 ms (69.60%) | Memory: 16.5 MB (20.82%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,6 +1,8 @@
 class Solution:
     #for set bits, we basically AND the  binary representation with n - 1
     # so evertime before adding to count, we do n = n & (n-1)
+    # offsets are updated based on most significant bit (MSB) which are in the form 1,2,4,8 
+    #so we check if offset*2 == i, then we update offset = i 
     def hammingWeight(self, n: int) -> int:
         count = 0
         while n > 0:
