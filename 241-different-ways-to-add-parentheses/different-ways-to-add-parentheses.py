288b23bd62e10917e0e64ed36e98b0b34776cd7f
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-09-16 20:51:20 -0400

    Time: 56 ms (9.87%) | Memory: 16.7 MB (27.91%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.py
@@ -0,0 +1,25 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        map = {}
+        if expression in map:
+            return map[expression]
+        result = []
+        for i in range(len(expression)):
+            char = expression[i]
+            if char in ['+', '-', '*']:
+                left = self.diffWaysToCompute(expression[:i])
+                right = self.diffWaysToCompute(expression[i+1:])
+                for  l in left:
+                    for r in right:
+                        if char == "+":
+                            result.append(l + r)
+                        elif char == "-":
+                            result.append(l - r)
+                        elif char == "*":
+                            result.append(l * r)
+        if len(result) == 0:
+            result.append(eval(expression))
+        map[expression] = result
+        return result
+
+                        
