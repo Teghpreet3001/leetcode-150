552813cbafb65ee748fec5441264fd39b09dba28
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-09-16 22:03:03 -0400

    Time: 189 ms (99.90%) | Memory: 17.9 MB (72.76%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.py
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -1,5 +1,8 @@
 class Solution:
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if len(matrix) == 0:
+            return 0
+            
         m = len(matrix)
         n = len(matrix[0])
         res = 0
