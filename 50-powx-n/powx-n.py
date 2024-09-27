9655e3cd09ff4aeafc69fea80663bc0faeb52c29
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-09-26 23:36:40 -0400

    Time: 31 ms (80.33%) | Memory: 16.6 MB (17.23%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,4 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        return self.binaryExp(x, n)
+    
+    #time and space complexity: O(logn)
+    def binaryExp(self, x, n): 
+        if n == 0:
+            return 1 
+        # if raised to negative number, we get the positive result and take the reciprocal of it
+        if n < 0:
+            return 1.0 / self.binaryExp(x, n * -1)
+
+        # if n is odd, we perform binary exponentiation to reduce the number of steps logarithmically on n - 1 and multiply  by x
+        # otherwise we calculate the result on n
+        if n % 2 == 1:
+            return x * self.binaryExp(x*x, (n-1)//2)
         
+        else:
+            return self.binaryExp(x*x, n//2)
