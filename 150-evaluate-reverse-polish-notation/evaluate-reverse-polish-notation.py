c7fcfaf0cf5e5d75fef5d7fa0a0c540667654a49
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-09-30 04:49:06 -0400

    Time: 60 ms (84.54%) | Memory: 17.1 MB (15.56%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def compute(self, num1, num2, operator):
+        result = 0
+        if operator == "+":
+            return num1 + num2
+        elif operator == "-":
+            return num1 - num2
+        elif operator == "*":
+            return num1 * num2
+        else:
+            return int(num1 / num2)
+        return result
+
+    def evalRPN(self, tokens: List[str]) -> int:
+        #stack to keep track of operated results
+        stack = []
+        for token in tokens:
+            #if token is a number, push it onto the stack
+            if token not in ["+", "-", "/", "*"]:
+                stack.append(int(token))
+                continue
+            #if token is an operator, pop the last two numbers num2 first then num1, compute and push onto stack
+            operator = token
+            num2 = stack.pop()
+            num1 = stack.pop()
+            result = self.compute(num1, num2, operator)
+            stack.append(int(result))
+        #return the last pushed result by doing return stack.pop()
+        return stack.pop()
+
+
+            
