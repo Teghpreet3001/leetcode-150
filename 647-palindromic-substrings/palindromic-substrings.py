2aed32e914c8b4ac9735c3259de639fc8aeae839
Teghpreet Singh Mago <87133706+Teghpreet3001@users.noreply.github.com>
2024-10-04 00:24:01 -0400

    Time: 149 ms (82.09%) | Memory: 16.5 MB (81.31%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -1,27 +1,18 @@
 class Solution:
-    @cache
     def countSubstrings(self, s: str) -> int:
-        #P[i][j] = whether or not i to j is a palindrome 
-        P = [[False for i in range(len(s))] for j in range(len(s))]
-        n = len(s)
-        result = 0
-
-        #minimum palindromic strings in a singe char is 1
-        for i in range(len(s)):
-            P[i][i] = True
-            result += 1
-
-        # Check for two character substrings
-        for i in range(n - 1):
-            if s[i] == s[i + 1]:
-                P[i][i + 1] = True
-                result += 1
-
-        # Check for substrings of length 3 or more
-        for length in range(3, n + 1):  # length is the length of the substring
-            for i in range(n - length + 1):
-                j = i + length - 1
-                if s[i] == s[j] and P[i + 1][j - 1]:
-                    P[i][j] = True
-                    result += 1
-        return result
+        n, ans = len(s), 0
+        
+        def palindromeCount(left: int, right: int) -> int:
+            count = 0
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+                count += 1
+            return count
+        
+        for i in range(n):
+            even = palindromeCount(i, i + 1)
+            odd = palindromeCount(i, i)
+            ans += even + odd
+            
+        return ans
